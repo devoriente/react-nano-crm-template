67c1c3a36c7c66312ad356227dd7d7c2a85a1018
devoriente <devoriente@outlook.com>
2022-08-27 16:20:29 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { FormEvent, useState } from "react";
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export type address = {
+    name: string;
+    email: string;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App: React.FC = () => {
+        const [name, setName] = useState("");
+        const [email, setEmail] = useState("");
+        const [addresses, setAddresses] = useState<address[]>([]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const newAddress: address = {
+            name,
+            email,
+        };
+        setAddresses([...addresses, newAddress]);
+        setName("");
+        setEmail("");
+    };
+
+    return (
+        <div>
+            <h1>Nano CRM</h1>
+                <form onSubmit={handleSubmit}>
+                    <p>
+                        <label htmlFor="name">Name : </label>
+                        <input id="name" value={name} onChange={e => setName(e.target.value)} />
+                    </p>
+                    <p>
+                        <label htmlFor="email">E-mail : </label>
+                        <input id="email" value={email} onChange={e => setEmail(e.target.value)} />
+                    </p>
+                    <p>
+                        <button type="submit">Add</button>
+                    </p>
+            </form>
+            <hr />
+            {addresses.length > 0 &&
+                (<ul>
+                    {addresses.map(element => <li key={element.email}>{element.name} : {element.email}</li>)}
+                </ul>
+                )
+            }
+        </div>
+    );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
